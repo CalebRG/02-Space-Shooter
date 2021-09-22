7d161ee978d37346f748fabcaa9063914d2bc52c
CalebRG <calebrg011@gmail.com>
2021-09-22 17:05:04 -0400

    2
    2

--- Enemy/Enemy.gd
+++ Enemy/Enemy.gd
@@ -3,9 +3,9 @@ extends KinematicBody2D
 var VP = Vector2.ZERO
 var direction = 0.0
 var dir_speed = 0.01
-var speed = 200
 var player = null
-
+var move = Vector2.ZERO
+var speed = 1
 
 var probability = 0.6
 
@@ -22,12 +22,16 @@ func _ready():
 	VP = get_viewport().size
 	randomize()
 
-func _physics_process(delta):
-	VP = Vector2.ZERO
-	if player:
-		VP = position.direction_to(player.position) * speed
-	VP = move_and_slide(VP)
-
+func _physics_process(_delta):
+	move = Vector2.ZERO
+	
+	if player != null:
+		move = position.direction_to(player.position) * speed
+	else:
+		move = Vector2.ZERO
+		
+	move = move.normalized()
+	move = move_and_collide(move)
 
 func die():
 	if Global.has_method("update_score"):
@@ -50,3 +54,14 @@ func _on_Timer_timeout():
 		bullet.rotation = direction
 		Bullets.add_child(bullet) 
 
+
+
+func _on_Area2D_body_entered(body):
+	if body != self:
+		player = body
+	else:
+		move = Vector2.ZERO
+
+
+func _on_Area2D_body_exited(body):
+	player = null
